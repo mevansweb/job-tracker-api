6ca1defcb605e2670115cff41ef0f70f54c419d1
mevansweb <michelle.n.evans@gmail.com>
2026-06-18 17:32:15 -0400

    remove deprecated body-parser import

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const bodyparser = require('body-parser')
 const express = require('express')
 const { MongoClient } = require("mongodb")
 const cors = require('cors')
@@ -15,9 +14,8 @@ const colName = 'myCollection'
 
 app.use(cors())
 //app.use(express.json())
-app.use(bodyparser.json({ limit: '50mb' }))
-app.use(bodyparser.urlencoded({ limit: '50mb', extended: true }))
-app.use(bodyparser.json())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
 app.use(function(req, res, next) {
   // TODO: update to match the domain you will make the request from
